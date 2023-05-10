9b6ec8162f2f88f6b97ea010e9de323d69ca4d37
Junhan <junhan@qiren.com.sg>
2023-05-10 17:51:33 +0800

    fix: url extra / && App starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	
+	const logo = <img key={0} src={reactLogo} className="logo react" alt="React logo" />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [array, setArray] = useState<React.ReactNode[]>([])
+
+	const handleClick = (): void => {
+		if (array.length === 4) {
+			setArray([]);
+			return;
+		}
+		const addLogo = <img key={array.length + 1} src={reactLogo} className="logo react" alt="React logo" />
+		setArray([...array, addLogo]);
+	};
+
+
+	return (
+		<>
+			<div>
+				{logo} {array}
+			</div>
+			<h1>Dashboard by Junhan.</h1>
+			<div className="card">
+				<button onClick={handleClick}>
+				Click to increase: {array.length + 1}
+				</button>
+			</div>
+		</>
+	)
 }
 
 export default App
