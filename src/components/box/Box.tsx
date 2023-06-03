c1c6a056e91d5a1df2600f3a9d5844f8097f0380
Junhan93 <junhan.3008@gmail.com>
2023-06-03 16:03:12 +0800

    fix: move pdf to public folder

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -10,7 +10,7 @@ const Box = () => {
 			<img key={0} src={reactLogo} className="logo" alt="React logo" />
 			<h2>Dashboard by Junhan.</h2>
 			<div className="box-column-layout">
-				<a href="./src/assets/data/resume/LiuJunHan_CV_LS.pdf" download>
+				<a href="./public/assets/data/LiuJunHan_CV_LS.pdf" download>
 					<button className="download-card resume">
 						<FaFileDownload style={{ color: '#40e0d0' }} size={36} />
 						<span className="text">Download<br/>Resume</span>
