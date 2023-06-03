f17f3d9925209e19a6090536e14cf8fbe0af21c9
Junhan93 <junhan.3008@gmail.com>
2023-06-03 17:04:24 +0800

    feat: alt way to view PDF

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -1,19 +1,28 @@
 import './Box.scss';
 import reactLogo from '../../assets/react.svg'
-import { FaFileDownload } from 'react-icons/fa';
+import { FaFileDownload, FaFilePdf } from 'react-icons/fa';
 
 
 const Box = () => {
+
+	const resumePathUrl = "https://junhan.io/public/assets/data/LiuJunHan_CV_LS.pdf";
 
 	return (
 		<div className="box">
 			<img key={0} src={reactLogo} className="logo" alt="React logo" />
 			<h2>Dashboard by Junhan.</h2>
 			<div className="box-column-layout">
-				<a href="./public/assets/data/LiuJunHan_CV_LS.pdf" download>
+				<a href={resumePathUrl} download="LiuJunHan_CV_LS.PDF">
+					<button className="download-card resume">
+						<FaFilePdf style={{ color: '#40e0d0' }} size={36} />
+						<span className="text">Download</span>
+					</button>
+				</a>
+
+				<a href={resumePathUrl} target="_blank">
 					<button className="download-card resume">
 						<FaFileDownload style={{ color: '#40e0d0' }} size={36} />
-						<span className="text">Download<br/>Resume</span>
+						<span className="text">Open File</span>
 					</button>
 				</a>
 			</div>
